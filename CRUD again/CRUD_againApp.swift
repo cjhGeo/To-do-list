94431ebeb8523f26cb730205066397ea0d064c07
cjhGeo <chong.jian.hang.geoffrey@sji.edu.sg>
2022-07-02 10:48:16 +0800

    Initial Commit

---
+++ CRUD again/CRUD_againApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD_againApp.swift
+//  CRUD again
+//
+//  Created by T Krobot on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_againApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
