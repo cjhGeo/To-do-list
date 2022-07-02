69ca5fe0b4e53783e8f47123c51edd471371f8c0
cjhGeo <chong.jian.hang.geoffrey@sji.edu.sg>
2022-07-02 10:57:34 +0800

    Imported content view

--- CRUD again/ContentView.swift
+++ CRUD again/ContentView.swift
@@ -1,16 +1,69 @@
 //
 //  ContentView.swift
-//  CRUD again
+//  CRUD_again
 //
-//  Created by T Krobot on 2/7/22.
+//  Created by T Krobot on 25/6/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var todos = [
+        Todo(title: "Watch some Paw Patrol"),
+        Todo(title: "Conduct a giveaway",
+             details: "??????"),
+        Todo(title: "Randomly deduct some points")]
+    
+    @State var isSheetPresented = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach($todos) { $todo in
+                    NavigationLink {
+                        TodoDetailView(todo: $todo)
+                    } label: {
+                        HStack {
+                            Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                            VStack(alignment: .leading) {
+                                Text(todo.title)
+                                    .foregroundColor(todo.isCompleted ? .green : .black)
+                                    .strikethrough(todo.isCompleted ? true : false)
+                                if !todo.details.isEmpty {
+                                    Text(todo.details)
+                                        .font(.caption)
+                                        .foregroundColor(.gray)
+                                }
+                            }
+                            .padding(7)
+                        }
+                    }
+                }
+                .onDelete { indexSet in
+                    todos.remove(atOffsets: indexSet)
+                }
+                .onMove { indices, newOffset in
+                    todos.move(fromOffsets: indices, toOffset: newOffset)
+                }
+            }
+            .navigationTitle("My important things")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        isSheetPresented = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
+        .sheet(isPresented: $isSheetPresented) {
+            NewTodoView(todos: $todos)
+        }
     }
 }
 
